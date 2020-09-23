1d79e5c662dfcb8c2eb400b673036f7475990d19
preetiverma525 <59359938+preetiverma525@users.noreply.github.com>
2020-09-23 13:44:17 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
+
+
+# In[ ]:
+
+
+
+
